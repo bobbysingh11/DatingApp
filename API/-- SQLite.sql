c3f74bba94eed1049942ca34404a22c26313b532
bobby <bobbycool005@gmail.com>
2023-02-12 14:55:24 +0530

    End of Section 2

---
+++ API/-- SQLite.sql
@@ -0,0 +1,12 @@
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (1,"Aman");
+
+INSERT INTO Users (Id, UserName)
+VALUES (2,"Sachin");
+
+INSERT INTO Users (Id, UserName)
+VALUES (3,"Rahul");
+
+INSERT INTO Users (Id, UserName)
+VALUES (4,"Rishub");
